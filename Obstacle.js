28cba4c8818c487abf6e5034f8d66b4f8fb6c2b1
Ethan Peterson <ethan@petetech.net>
2019-01-01 18:48:50 -0500

    fixed a crash

--- Obstacle.js
+++ Obstacle.js
@@ -1,5 +1,12 @@
-class Obstacle {
-  constructor() {
-    
-  }
+function Obstacle() {
+  //private var drawIndex = 0;
+  this.jacket1 = loadImage('jacket1.png');
+  this.drawings = [
+    function() {
+      image(this.jacket1, 100, 100);
+    },
+    function() {
+      print("Second JACKET");
+    }
+  ];
 }
--- dodge_the_gpa.js
+++ dodge_the_gpa.js
@@ -1,16 +1,25 @@
 var img;
 
 var backColor = 155;
-
+var obstacles = [0, 0, 0, 0]; // max of 4 obstacles at any given time.
 function setup() {
   createCanvas(800, 600);
   background(backColor);
   img = loadImage('tam.png');
+  getObstacles();
+  //obstacles[0].drawings[0]();
+  obstacles[0].drawings[1]();
 }
 
 function draw() {
   background(backColor);
   image(img, mouseX, mouseY, img.width / 3, img.height / 3);
+}
+
+function getObstacles() {
+  for (var i = 0; i < 4; i = i + 1) {
+    obstacles[i] = new Obstacle();
+  }
 }
 
 function drawHUD() {
