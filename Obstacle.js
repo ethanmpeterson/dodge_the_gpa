69713f0cfb70053f4aa93fe0188d1273feea0e86
Ethan Peterson <ethan@petetech.net>
2019-01-02 19:25:15 -0500

    worked on obstacle class

--- Obstacle.js
+++ Obstacle.js
@@ -1,12 +1,39 @@
-function Obstacle() {
-  //private var drawIndex = 0;
-  this.jacket1 = loadImage('jacket1.png');
-  this.drawings = [
-    function() {
-      image(this.jacket1, 100, 100);
-    },
-    function() {
-      print("Second JACKET");
+function Obstacle(img) {
+  // Constructor code
+  this.img = img;
+  this.possibleX = [900, -100];
+  this.possibleY = [-100, 700];
+  
+  this.y = 0;
+  this.x = 0;
+  
+  this.mode = 0;
+
+  this.xIdx = random(1);
+  this.yIdx = random(1);
+  this.x = this.possibleX[this.xIdx];
+  this.y = this.possibleY[this.yIdx];
+  
+  // test
+  this.x = 400;
+  this.y = 300;
+
+  this.update = function() {
+    // ADD HIT DETECTION USING TAM POS
+    
+    // after collision check incriment position
+    if (this.xIdx == 0) {
+      this.x = this.x - 1;
+    } else if (this.xIdx == 1) {
+      this.x = this.x + 1;
     }
-  ];
+
+    if (this.yIdx == 0) {
+      this.y = this.y + 1;
+    } else if (this.yIdx == 1) {
+      this.y = this.y - 1;
+    }
+    image(this.img, this.x, this.y);
+  }
+  
 }
--- dodge_the_gpa.js
+++ dodge_the_gpa.js
@@ -1,3 +1,4 @@
+var imgs;
 var img;
 
 var backColor = 155;
@@ -6,19 +7,24 @@ function setup() {
   createCanvas(800, 600);
   background(backColor);
   img = loadImage('tam.png');
+  imgs = [
+    loadImage('jacket1.png'),
+    loadImage('jacket2.png'),
+    loadImage('jacket3.gif')
+  ];
+  print(imgs);
   getObstacles();
-  //obstacles[0].drawings[0]();
-  obstacles[0].drawings[1]();
 }
 
 function draw() {
   background(backColor);
+  obstacles[0].update();
   image(img, mouseX, mouseY, img.width / 3, img.height / 3);
 }
 
 function getObstacles() {
   for (var i = 0; i < 4; i = i + 1) {
-    obstacles[i] = new Obstacle();
+    obstacles[i] = new Obstacle(imgs[i]);
   }
 }
 
